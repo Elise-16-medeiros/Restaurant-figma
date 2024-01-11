dcc033833cf41881d27f1a3612c8396d80f76128
Elise-16-medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-01-11 10:37:25 -0300

    feat: installing fonts

---
+++ restaurant-figma/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Title</h1>
+      <p>something is happens</p>
+      <p>Home</p>
+      <p>About Us</p>
+      <button>Menu</button>
+    
+    </main>
+  )
+}
