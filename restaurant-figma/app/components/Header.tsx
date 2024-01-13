af6b082e48f0c26595ae14cdb215a71f2e3a937a
Elise-16-medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-01-13 13:33:13 -0300

    feat: configuration of header

---
+++ restaurant-figma/app/components/Header.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png";
+
+function Header() {
+  return (
+    <header className="w-full h-72 flex justify-center items-center bg-[#292E36] md:h-[180px]">
+      <div className="flex flex-col items-center md:flex-row">
+        <span className="px-4 py-1 text-white border-[#E1B168] border-2 rounded">
+          Call - 123 456 789
+        </span>
+        <Image src={Logo} width={200} height={200} alt="logo" />
+        <Link
+          className="link-custom-two"
+          href="/reservation"
+        >
+          reservation
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- restaurant-figma/app/page.tsx
+++ restaurant-figma/app/page.tsx
@@ -1,13 +1,10 @@
+import Header from "./components/Header";
 
 
 export default function Home() {
   return (
     <main>
-      <h1>Title</h1>
-      <p>something is happens</p>
-      <p>Home</p>
-      <p>About Us</p>
-      <button>Menu</button>
+      <Header/>
     
     </main>
   )
