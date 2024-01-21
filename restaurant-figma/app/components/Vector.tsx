431a51a6c7b87370491ca92114445e427855a9cc
Elise-16-medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-01-21 02:09:05 -0300

    feat:reparations

--- restaurant-figma/app/components/Vector.tsx
+++ restaurant-figma/app/components/Vector.tsx
@@ -5,9 +5,68 @@ import Devices from "@/public/devices_vector.png";
 
 function Vector() {
   return (
-    <section className="container bg-[#FFF8F5] w-full h-[340px] md:h-[230px] px-10">
+    <section className="bg-[#FFF8F5] w-full h-[340px] md:h-[230px]">
+      <div className="container flex flex-col justify-around items-center gap-8 pt-20 px-5 md:flex-row">
+      
+        <div className="flex gap-3">
+          <ul>
+            <li>
+            <Image
+            className="h-[45px] w-[45px] rounded-lg"
+            src={Pin}
+            alt="pin image"
+          />
+            </li> 
+          </ul>
+          <ul>
+            <li>
+              <h3 className="text-xl tracking-wide font-semibold">Locate Us</h3>
+            </li>
+            <li>Via Cavour 25, Florence, Tuscany</li>
+          </ul>
+        </div>
 
-      <div className="pt-20 flex flex-col justify-center items-center gap-y-10 md:flex-row md:gap-x-20">
+        <div className="flex gap-3">
+          <ul>
+             <li>
+            <Image
+            className="h-[45px] w-[45px] rounded-lg"
+            src={Clock}
+            alt="pin image"
+          />
+            </li> 
+          </ul>
+          <ul>
+            <li>
+              <h3 className="text-xl tracking-wide font-semibold">Open Hours</h3>
+            </li>
+            <li>Mon To Sat 18:00 PM - 23:00 PM</li>
+          </ul>
+        </div>
+
+        <div className="flex gap-3">
+          <ul>
+             <li>
+            <Image
+            className="h-[45px] w-[45px] rounded-lg"
+            src={Devices}
+            alt="pin image"
+          />
+            </li> 
+          </ul>
+          <ul>
+            <li>
+              <h3 className="text-xl tracking-wide font-semibold">Reservation</h3>
+            </li>
+            <li>reservation_botticelli@gmail.com</li>
+          </ul>
+        </div>
+
+
+
+      </div>
+
+      {/*   <div className="pt-20 flex flex-col justify-center items-center gap-y-10 md:flex-row md:gap-x-20">
         <div className="flex gap-2">
            <Image
             className="h-[50px] w-[50px] rounded-lg"
@@ -44,11 +103,9 @@ function Vector() {
             <li><p className="text-[14px]">restaurant@gmail.com</p></li>
           </ul>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
 
 export default Vector;
-
-
